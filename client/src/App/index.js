19677b3034aff885f727a6f97b5ed1fe10e027e1
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-07-15 00:06:31 -0700

    Remove fragment

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -11,20 +11,18 @@ export const App = () => {
     // todo: add react router and other pages
     return (
         <BrowserRouter>
-            <>
-                <Switch>
-                    <Route path="/chat">
-                        <Chat />
-                    </Route>
-                    <Route path="/newcall">
-                        <div style={{ color: 'white' }}>New call page</div>
-                    </Route>
-                    <Route exact path="/">
-                        <div style={{ color: 'white' }}>home page</div>
-                    </Route>
-                    <Redirect to='/' />
-                </Switch>
-            </>
+            <Switch>
+                <Route path="/chat">
+                    <Chat />
+                </Route>
+                <Route path="/newcall">
+                    <div style={{ color: 'white' }}>New call page</div>
+                </Route>
+                <Route exact path="/">
+                    <div style={{ color: 'white' }}>home page</div>
+                </Route>
+                <Redirect to='/' />
+            </Switch>
         </BrowserRouter>
     );
 }
